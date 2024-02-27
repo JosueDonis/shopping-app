a3b573ef54628604b6c6c11d0921032eac0e438b
JoshDonis <bjosh.donis@gmail.com>
2024-02-27 04:02:55 -0600

    Add @vitejs/plugin-legacy and rewrite rules for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 import vercel from 'vite-plugin-vercel';
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel(),],
+  plugins: [
+    react(), 
+    vercel(), 
+    legacy(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
