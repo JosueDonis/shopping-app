a3942f894d34e3363d842f03e1226fba7c60ec3a
JoshDonis <bjosh.donis@gmail.com>
2024-02-27 04:13:07 -0600

    Update Vercel configuration and comment out legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
-import vercel from 'vite-plugin-vercel';
-import legacy from '@vitejs/plugin-legacy'
+// import vercel from 'vite-plugin-vercel';
+// import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    legacy(),
-    vercel(), 
+    // legacy(),
+    // vercel(), 
   ],
   resolve: {
     alias: {
