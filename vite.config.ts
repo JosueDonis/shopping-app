084458361f6caf5fc9374d4fb4005e22fb6e5155
JoshDonis <bjosh.donis@gmail.com>
2024-02-27 10:51:05 -0600

    Update dependencies and remove unused plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
-// import vercel  from 'vite-plugin-vercel';
-// import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    // legacy(),
-    // vercel(), 
-  ],
-  appType: "spa",
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@helpers': path.resolve(__dirname, './src/helpers'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@context': path.resolve(__dirname, './src/context'),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@helpers": path.resolve(__dirname, "./src/helpers"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@context": path.resolve(__dirname, "./src/context"),
+    },
+  },
+});
