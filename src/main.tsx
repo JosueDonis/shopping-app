b2f35e0f65147612fad922ed395569a790b14ebc
JoshDonis <bjosh.donis@gmail.com>
2024-02-27 11:24:31 -0600

    Commented out unused imports and components

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/styles/index.css";
-import { Shopping } from "@components/templates";
+// import { Shopping } from "@components/templates";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "@context/ThemeProvider";
-import { Provider } from "react-redux";
-import { store } from "@store/store";
+// import { ThemeProvider } from "@context/ThemeProvider";
+// import { Provider } from "react-redux";
+// import { store } from "@store/store";
 import router from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
