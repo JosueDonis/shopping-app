f3e407e84a8647bacae9bf49a1c45848659a6145
JoshDonis <bjosh.donis@gmail.com>
2024-02-28 22:09:26 -0600

    Refactor CartList component

--- src/components/organisms/CartList.tsx
+++ src/components/organisms/CartList.tsx
@@ -1,13 +1,17 @@
 import { CartProductType } from "@/types/cart";
 import { CartCard } from "../molecules/CartCard";
-import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 type CartListProps = {
   products?: CartProductType[];
   onRemove?: (id?: string) => void;
   register?: any;
   errors?: any;
-  onChangeQuantity?: (value?: string | number | undefined, id?: string, index?: number) => void;
+  onChangeQuantity?: (
+    value?: string | number | undefined,
+    id?: string,
+    index?: number
+  ) => void;
 };
 
 export const CartList: React.FC<CartListProps> = ({
@@ -17,27 +21,26 @@ export const CartList: React.FC<CartListProps> = ({
   register,
   errors,
 }) => {
-  const [cartList,] = useAutoAnimate({
-  duration: 200,
-  })
+  const [cartList] = useAutoAnimate({
+  });
   return (
     <div className="flex flex-col gap-4 mt-8">
       <h2 className="text-xl font-bold">Productos</h2>
       <ul ref={cartList}>
-      {products?.map?.((product: CartProductType, index: number) => (
-        <CartCard
-          productId={""}
-          key={product.id}
-          {...product}
-          onRemove={() => {
-            onRemove?.(product.productId);
-          }}
-          register={register}
-          onChangeQuantity={onChangeQuantity}
-          index={index}
-          errors={errors.products?.[index]}
-        />
-      ))}
+        {products?.map?.((product: CartProductType, index: number) => (
+          <CartCard
+            productId={""}
+            key={product.id}
+            {...product}
+            onRemove={() => {
+              onRemove?.(product.productId);
+            }}
+            register={register}
+            onChangeQuantity={onChangeQuantity}
+            index={index}
+            errors={errors.products?.[index]}
+          />
+        ))}
       </ul>
     </div>
   );
